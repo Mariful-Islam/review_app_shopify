4978c1be413c2679ad59f1dee770f7a08829a554
Mariful-Islam <marifulesgiu@gmail.com>
2025-08-19 12:59:29 +0600

    first commit

---
+++ app/routes/app.setting.jsx
@@ -0,0 +1,37 @@
+import {
+  Box,
+  Card,
+  Layout,
+  Link,
+  List,
+  Page,
+  Text,
+  BlockStack,
+} from "@shopify/polaris";
+import { TitleBar } from "@shopify/app-bridge-react";
+
+export default function SettingPage() {
+  return (
+    <Page>
+      <TitleBar title="Setting" />
+     
+    </Page>
+  );
+}
+
+function Code({ children }) {
+  return (
+    <Box
+      as="span"
+      padding="025"
+      paddingInlineStart="100"
+      paddingInlineEnd="100"
+      background="bg-surface-active"
+      borderWidth="025"
+      borderColor="border"
+      borderRadius="100"
+    >
+      <code>{children}</code>
+    </Box>
+  );
+}
